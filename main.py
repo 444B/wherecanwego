5d3e7e6524ba06d6b32107fbd5ccf77ca77a2f86
444B <15311579+444B@users.noreply.github.com>
2025-01-09 23:37:07 +0000

    shows acceptable visa countries

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ all_visas_statuses = [
     "e-visa",
     "visa required",
     "no admission",
-]
-all_duration_lengths = [
     "7",
     "10",
     "14",
@@ -36,7 +34,6 @@ all_duration_lengths = [
     "240",
     "360",
 ]
-
 
 # allows the user to select their passports
 def select_passport_names() -> list:
@@ -60,17 +57,10 @@ def select_passport_isos(passport_names: list) -> list:
 def select_acceptable_visas(all_visas_statuses: list) -> list:
 
     acceptable_visas_statuses = st.multiselect(
-        "Acceptable Visas", all_visas_statuses, "visa free"
+        "Acceptable Visas", all_visas_statuses, "visa free", help="Number mean visa-free days, where available "
     )
     return acceptable_visas_statuses
 
-
-# Takes in a list of all the possible dates and lets the user select their choices
-def select_acceptable_dates(all_visas_statuses: list) -> list:
-    acceptable_visas_statuses = st.select_slider(
-        "Acceptable Duration", all_visas_statuses, value=("30", "90")
-    )
-    return acceptable_visas_statuses
 
 
 def main() -> None:
@@ -95,8 +85,6 @@ def main() -> None:
         with col2:
             st.write("Visas")
             acceptable_visas = select_acceptable_visas(all_visas_statuses)
-            st.write("OR")
-            acceptable_length = select_acceptable_dates(all_duration_lengths)
 
     # Section to display the data
     with st.expander("Results", expanded=True):
@@ -105,12 +93,22 @@ def main() -> None:
         # this shows the entire data we have on which countries accept visas from each other
         visa_df = pd.read_csv("visa_data.csv")
 
-        # this shows the info for the countries selected
+        # this shows the Rows info for the countries selected
         filtered_df = visa_df[visa_df["Passport"].isin(passport_isos)]
-        filtered_df
 
-        # TODO We need to adjust filtered_df even further to only show countries
-        # that are in in the acceptable_visas list
+
+        valid_columns = ['Passport']
+
+#
+        for col in filtered_df.columns:
+            if col != 'Passport':  # Skip the Passport column
+        #         # Check if any value in this column matches our criteria
+                if filtered_df[col].isin(acceptable_visas).all():
+                    valid_columns.append(col)
+
+        # Final filtered dataframe
+        result_df = filtered_df[valid_columns]
+        result_df
 
         # TODO Add error handling for countries for which there is no data
         # Examples: Aland Islands, Antarctica, American Samoa
