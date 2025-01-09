3b22fc779b8bfefc916b835e2eb5a98ddadabf4d
444B <15311579+444B@users.noreply.github.com>
2025-01-09 20:44:07 +0000

    first working version

---
+++ README.md
@@ -0,0 +1,45 @@
+# Contributing to WhereCanWeGo
+
+## Getting Started
+1. Fork and clone:
+```sh
+git clone https://github.com/444B/wherecanwego.git
+cd wherecanwego
+```
+2. Create branch:
+```sh
+git checkout -b your-feature-name
+```
+
+## Development Environment Setup
+1. Install Python 3.10.x
+2. Install and setup uv:
+```sh
+pip install uv
+uv venv .venv --python 3.10.16
+source .venv/bin/activate
+```
+3. Install dependencies:
+```sh
+uv pip install -e ".[dev]"
+```
+4. Run the app:
+```sh
+streamlit run main.py
+```
+5. If you need to add a package:
+```sh
+uv add <package-name>
+```
+
+## Pull Request Process
+
+1. Push changes:
+```sh
+git add .
+git commit -m "Your commit message"
+git push origin your-feature-name
+```
+2. Open PR on GitHub
+
+For issues: [GitHub issue tracker](https://github.com/444B/wherecanwego/issues/new/choose)
--- main.py
+++ main.py
@@ -1,55 +1,75 @@
 # data comes from https://github.com/ilyankou/passport-index-dataset/blob/master/passport-index-matrix-iso2.csv
 
 
-import streamlit as st
-import pandas as pd
 import json
 
-# load the countries to iso
-with open('countries_2_iso.json', 'r') as f:
+import pandas as pd
+import streamlit as st
+
+# read json data
+with open("country_2_iso.json", "r") as f:
     json_data = json.load(f)
 
-countries_dict = json_data
+# list of acceptable visa statuses
+acceptable_visas_statuses = [
+    "visa free",
+    "visa on arrival",
+    "e-visa",
+    "30",
+    "90",
+    "180",
+]
+# TODO make this ^ a selectable list so that users can decide for themselves what is acceptable
 
-df = pd.DataFrame({'Country': json_data.values(), 'Key': json_data.keys()})
+
+def select_passport_name():
+    list_of_country_names = st.multiselect("Issuing Country:", json_data.values())
+
+    return list_of_country_names
 
 
-def add_passport():
-    country = st.multiselect("Issuing Country:",countries_dict.values())
-    return country
-
-
+def select_passport_isos(passport_names):
+    isos = []
+    for passport in passport_names:
+        for iso, country in json_data.items():
+            if country == passport:
+                isos.append(iso)
+    return isos
 
 
 def main():
-    st.header("Passport Compatible Matrix")
+    st.header("Where can we go bro")
 
+    # Create a section for selecting passport and a button to add more
     with st.container():
         col1, col2 = st.columns([8, 1], border=True)
-        
-        with col1:
-            passport = add_passport()
 
+        # Select Passport
+        with col1:
+            passport_names: list = select_passport_name()
+            passport_isos: list = select_passport_isos(passport_names)
+
+        # Button to add another passport
         with col2:
             if st.button("\+"):
-                add_passport()
+                pass  # TODO
 
-    
+    # Section to display the data
+    with st.expander("Results", expanded=True):
+        st.write(f"You have selected {passport_names}")
 
-    with st.expander("Results",expanded=True):
-        st.write(f"You have selected {passport}")
+        # this shows the entire data we have on which countries accept visas from each other
+        visa_df = pd.read_csv("visa_data.csv")
 
-        data_df = pd.read_csv("country_data.csv")
-        data_df
+        # this shows the info for the countries selected
+        filtered_df = visa_df[visa_df["Passport"].isin(passport_isos)]
+        filtered_df
 
+        # TODO We need to adjust filtered_df even further to only show countries
+        # that are in acceptable_visa_status[]
 
-
-    df
-
-
-def get_passport() -> str:
-    return 
-
+        # TODO Add error handling for countries for which there is no data
+        # Examples: Aland Islands, Antarctica, American Samoa
 
 
 if __name__ == "__main__":
